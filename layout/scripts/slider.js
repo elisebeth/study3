0ca3474bc56cd5b24e6f4241d8b05418e4af8010
Alexander Raldugin <elisebeth@pop-os.localdomain>
2021-03-20 00:48:33 +0300

    add adaptive

--- layout/scripts/slider.js
+++ layout/scripts/slider.js
@@ -1,5 +1,9 @@
-let items = document.querySelectorAll(".bottom-btn");
+let main_content = document.querySelector(".header__main-content");
+let button_box = document.querySelector(".header__bottom-buttons");
 
-for (let i = 0; i < items.length; ++i) {
-  // console.log(i, " ");
-}
+let menu_btn = document.querySelector(".menu__btn");
+
+menu_btn.addEventListener("click", () => {
+  main_content.classList.toggle("hidden");
+  button_box.classList.toggle("hidden");
+});
